25698d2f9fb88208a1650f69a4bf329f9b780f3a
Santosh Singh <santosh007bca@gmail.com>
2023-11-16 12:47:02 +0530

    install react-js tailwind css templete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About-us" element={<About />} />
+        <Route path="/user-profile" element={<Profile />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
